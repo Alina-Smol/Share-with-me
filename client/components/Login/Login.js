5da6e9741f1d525acc4f971e1b4764555bc16be2
Lisa-creates <77747128+Lisa-creates@users.noreply.github.com>
2022-11-16 21:15:59 +0300

    Add files via upload

---
+++ client/components/Login/Login.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
+
+import './Login.scss'
+
+import logo from './logoPng.png'
+
+function Login() {
+
+    const [emailval,setemailval]=useState("")
+    const [passval,setpassval]=useState("")
+
+
+    const handlesubmit=(event)=>{
+        event.preventDefault()
+    }
+
+    return(
+        <div className='main-login'>
+            <div className= 'login-contain'>
+                <div className="left-side">
+                    <div className="img-class">
+                        <img src={logo} id="img-id"/>
+                    </div>
+                    <h3>Вход</h3>
+                    <form onSubmit={handlesubmit}>
+                        <label for="emil1">Логин</label>
+                        <input placeholder="Введите свой email" type="email" value={emailval}
+                            onChange={(e)=>{setemailval(e.target.value)}}
+                               id="emil1"/>
+                        <label for="pwd1">Пароль</label>
+                        <input placeholder="Введите свой пароль" type="password"
+                        value={passval} onChange={(e)=>{setpassval(e.target.value)}}
+                               id="pwd1"/>
+                        <button type="submit" id="sub_butt">Войти</button>
+                    </form>
+                    <div className="footer">
+                        <h4> Нет аккаунта?  <Link to="/Register"> Создайте </Link>
+                        </h4>
+                        <Link to="/User_profile"> Проверка </Link>
+                    </div>
+                </div>
+{/*
+                <div className="right-side"></div>
+*/}
+
+            </div>
+        </div>
+    )
+}
+
+export default Login;
